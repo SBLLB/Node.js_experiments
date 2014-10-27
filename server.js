027690f9a2ce5040433e2b4605479486a25d1734
RachelNolan <mail@rachelnolan.com>
2014-10-27 12:49:03 +0000

    Simple server module

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {
+		"Content-Type": "text/plain"
+	});
+	response.write("Testing out Node.js");
+	response.end();
+}).listen(8888);
+
+
